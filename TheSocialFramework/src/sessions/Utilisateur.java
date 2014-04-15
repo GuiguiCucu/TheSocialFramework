045db01e3f67c95278c6fa7944521250c124a5c0
cutroneg <guillaume.cutrone@gmail.com>
2014-04-15 11:01:52 +0200

    classe utilisateur

--- TheSocialFramework/src/sessions/Utilisateur.java
+++ TheSocialFramework/src/sessions/Utilisateur.java
@@ -2,6 +2,7 @@ package sessions;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class Utilisateur {
 
@@ -31,23 +32,14 @@ public class Utilisateur {
 	public String getLogin() {
 		return login;
 	}
-
-	/**
-	 * Mutateur de login
-	 * 
-	 * @param login
-	 */
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
+	
 	/**
 	 * Encode le String passé en paramètre en MD5
 	 * 
 	 * @param password
 	 * @return
 	 */
-	private String encode(String password) {
+	private static String encode(String password) {
 		byte[] uniqueKey = password.getBytes();
 		byte[] hash = null;
 
@@ -67,6 +59,32 @@ public class Utilisateur {
 				hashString.append(hex.substring(hex.length() - 2));
 		}
 		return hashString.toString();
+	}
+	
+	/**
+	 * Permet de verifier la presence d'un utilisateur dans l'ArrayList listUsers
+	 * Fonction de verification pour la gestion des utilisateurs
+	 * @param login
+	 * @param password
+	 * @param listUsers ArrayList<Utilisateur>
+	 * @return l'utilisateur courant si présent dans listUsers, null sinon
+	 */
+	public static Utilisateur verification(String login, String password, ArrayList<Utilisateur> listUsers){
+
+		boolean connected = false;
+		
+		for (int i = 0; i < listUsers.size(); i++) {
+			if (listUsers.get(i).getLogin().equals(login)
+					&& listUsers.get(i).getPassword()
+							.equals(encode(password))) {
+				connected = true;
+			}
+		}
+		
+		if(connected)
+			return new Utilisateur(login, password);
+		else
+			return null;
 	}
 
 }
--- TheSocialFramework/src/sessions/tests/MainTest.java
+++ TheSocialFramework/src/sessions/tests/MainTest.java
@@ -2,22 +2,30 @@ package sessions.tests;
 
 import java.util.ArrayList;
 
+import javax.rmi.CORBA.Util;
+
 import sessions.Utilisateur;
 
 public class MainTest {
-	
+
 	private ArrayList<Utilisateur> listUsers;
 
 	public static void main(String[] args) {
 		new MainTest();
 	}
-	
+
 	public MainTest() {
+		boolean connected = false;
+
 		listUsers = new ArrayList<Utilisateur>();
 		Utilisateur user1 = new Utilisateur("michel", "micheldu38");
-		
-		listUsers.add(user1);
-		System.out.println(user1.getPassword());
-	}
 
+		listUsers.add(user1);
+
+		Utilisateur currentUser = Utilisateur.verification("michel", "micheldu38", listUsers);
+		if(currentUser != null)
+			System.out.println("utilisateur connecté");
+		else 
+			System.out.println("login ou mot de passe incorrect");
+	}
 }
