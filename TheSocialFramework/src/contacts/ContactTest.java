fa37773708645d1dd17a2934c1b63372cc825033
montalda <montalda@PCNT214-07.e-ima.ujf-grenoble.fr>
2014-04-15 16:23:54 +0200

    Fin des imports de contacts

--- TheSocialFramework/src/contacts/ContactTest.java
+++ TheSocialFramework/src/contacts/ContactTest.java
@@ -3,7 +3,11 @@ package contacts;
 public class ContactTest {
 
 	public static void main(String[] args) {
-		Contacts.importer("Z:/POO/ProjetFramework/google.csv");
+		Contacts.importer("Z:/POO/ProjetFramework/OutlookContacts.csv");
+		
+		for(int i=0; i<Contacts.contacts.size(); i++){
+			System.out.println(((Personne) Contacts.contacts.get(i)).getAdresse());
+		}
 	}
 
 }
