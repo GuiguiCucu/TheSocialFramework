0b073c4e0e91d1307215dd1842f5b800ea9e18f2
Gwenael <Gwenael@130.190.29.12>
2014-04-15 11:24:40 +0200

    début d'annuaire

---
+++ TheSocialFramework/src/annuaire/Annuaire.java
@@ -0,0 +1,19 @@
+package annuaire;
+
+import java.util.ArrayList;
+
+import sessions.Utilisateur;
+
+public class Annuaire {
+
+	private ArrayList<Utilisateur> annuaire ;
+	
+	/**
+	 * Construit un annuaire vide
+	 */
+	public Annuaire(){
+		this.annuaire = new ArrayList<Utilisateur>();
+	}
+	
+	public Annuaire()
+}
