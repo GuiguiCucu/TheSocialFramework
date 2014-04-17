741f886c3326e4a61c79d39e536bea3e0c4cd7a9
cutroneg <guillaume.cutrone@gmail.com>
2014-04-17 13:08:02 +0200

    dropbox test

---
+++ TheSocialFramework/src/dropbox/tests/TestDropbox.java
@@ -0,0 +1,12 @@
+package dropbox.tests;
+
+public class TestDropbox {
+
+	public static void main(String[] args) {
+		String APP_KEY = "INSERT_APP_KEY";
+	    String APP_SECRET = "INSERT_APP_SECRET";
+	    
+	    // appel de la methode ...
+	}
+
+}
