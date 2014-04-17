20f50c092e9e9f4060f9138ce50c5831daaf6b43
cutroneg <guillaume.cutrone@gmail.com>
2014-04-17 15:27:02 +0200

    htrgdhgdfhgd

--- TheSocialFramework/src/contacts/Groupe.java
+++ TheSocialFramework/src/contacts/Groupe.java
@@ -1,32 +1,12 @@
 package contacts;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Groupe extends Contacts {
 
-/**
- * Possibilité de classer les contacts dans des groupes
- * @author montalda
- *
- */
-public class Groupe {
-	
 	private String nom;
-	private List liste;
 	
-	/**
-	 * Constructeur d'un groupe de contact
-	 * @param nom
-	 */
-	public Groupe(String nom){
+	public Groupe(String nom) {
+		super();
 		this.nom = nom;
-		this.liste = new ArrayList<Personne>();
 	}
-	
-	/**
-	 * Ajoute une personne au groupe de contact
-	 * @param p
-	 */
-	public void ajouterPersonne(Personne p){
-		liste.add(p);
-	}
+
 }
--- TheSocialFramework/src/contacts/tests/ContactTest.java
+++ TheSocialFramework/src/contacts/tests/ContactTest.java
@@ -1,6 +1,7 @@
 package contacts.tests;
 
 import contacts.Contacts;
+import contacts.Groupe;
 import contacts.Personne;
 
 public class ContactTest {
@@ -8,20 +9,24 @@ public class ContactTest {
 	public static void main(String[] args) {
 		
 		Contacts contacts = new Contacts();
+		Groupe groupe = new Groupe("Mes bestos");
 		
 		// test de l'import
-		contacts.importer("Z:/POO/ProjetFramework/google.csv");
-		//contacts.importer("/home/c/cutroneg/PPO/google.csv");
+		//contacts.importer("Z:/POO/ProjetFramework/google.csv");
+		groupe.importer("/home/c/cutroneg/PPO/google.csv");
 		
-		for(int i=0; i<contacts.getContacts().size(); i++){
-			System.out.print(((Personne) contacts.getContacts().get(i)).getNom()+" ");
-			System.out.println(((Personne) contacts.getContacts().get(i)).getPrenom());
-			System.out.println(((Personne) contacts.getContacts().get(i)).getAdresse());
+		for(int i=0; i<groupe.getContacts().size(); i++){
+			System.out.print(((Personne) groupe.getContacts().get(i)).getNom()+" ");
+			System.out.println(((Personne) groupe.getContacts().get(i)).getPrenom());
+			System.out.println(((Personne) groupe.getContacts().get(i)).getAdresse());
 			System.out.println("\n*****************\n");
 		}
 		
+		//System.out.println(groupe.findPersonneByPrenom("Adèle").get(0).getNom());
+
+		
 		// test de l'export
-		 contacts.exporter("Z:/POO/ProjetFramework/test.csv");
+		//contacts.exporter("Z:/POO/ProjetFramework/test.csv");
 		//contacts.exporter("/home/c/cutroneg/PPO/contacts.csv");
 	}
 
