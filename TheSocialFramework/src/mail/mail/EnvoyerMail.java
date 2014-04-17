22ee2832bb84aa122961409bc1726479d9013caa
imag-soft <imag-soft@imagsoft-Latitude-E6500>
2014-04-17 21:43:50 +0200

    Ayé les mails marchent de partout normalement

--- TheSocialFramework/src/mail/mail/EnvoyerMail.java
+++ TheSocialFramework/src/mail/mail/EnvoyerMail.java
@@ -1,10 +1,11 @@
-package mail;
+package mail.mail;
 
 import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
@@ -12,34 +13,38 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class EnvoyerMail {
-	public void envoyerMail(String destinataire, String sujet, String corps) {
+	public static void envoyerMail(String destinataire, String sujet,
+			String corps) {
 		final String user = "theSocialFramework";
 		final String pwd = "Miage2014";
 		final String adMail = "theSocialFramework@laposte.net";
-		final String MAILER_VERSION = "Java";
 		final String serveur = "smtp.laposte.net";
-		final int port = 465;
+		final int port = 587;
 		try {
-			Properties prop = System.getProperties();
-			prop.put("smtp.laposte.host", serveur);
-			prop.put("smtp.laposte.auth", "true");
-			prop.put("smtp.laposte.port", port);
-			Session session = Session.getInstance(prop);
-			Message message = new MimeMessage(session);
+			Properties prop = new Properties();
+			prop.setProperty("mail.transport.protocol", "smtp");
+			prop.setProperty("mail.smtp.host", serveur);
+			prop.setProperty("mail.smtp.user", user);
+			prop.setProperty("mail.from", adMail);
+			prop.setProperty("mail.smtp.auth", "true");
+			prop.put("mail.smtp.password", pwd);
+
+			Session session = Session.getInstance(prop,
+					new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(user, pwd);
+						}
+					});
+			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(adMail));
-			InternetAddress[] internetAddresses = new InternetAddress[1];
-			internetAddresses[0] = new InternetAddress(destinataire);
-			message.setRecipients(Message.RecipientType.TO, internetAddresses);
+			message.setRecipients(Message.RecipientType.TO, destinataire);
 			message.setSubject(sujet);
 			message.setText(corps);
-			message.setHeader("X-Mailer", MAILER_VERSION);
 			message.setSentDate(new Date());
-			System.out
-					.println(("Essai de Mail a " + destinataire + " de : " + adMail));
+
 			Transport transport = session.getTransport("smtp");
 			transport.connect(serveur, port, user, pwd);
-			transport.sendMessage(message, message.getAllRecipients());
-			transport.close();
+			Transport.send(message);
 		} catch (AddressException e) {
 			e.printStackTrace();
 		} catch (MessagingException e) {
---
+++ TheSocialFramework/src/mail/tests/MailTest.java
@@ -0,0 +1,16 @@
+package mail.tests;
+
+import mail.mail.EnvoyerMail;
+
+public class MailTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EnvoyerMail.envoyerMail("gcharlety@laposte.net", "SujetTest",
+				"Corps  Tests éà");
+	}
+
+}
