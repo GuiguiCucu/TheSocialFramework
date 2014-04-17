c9ca644b3bc7d5918d8b8cd6d2325d6be4f6532c
montalda <montalda@PCNT214-07.e-ima.ujf-grenoble.fr>
2014-04-17 13:24:30 +0200

    separation fonctions dropbox

--- TheSocialFramework/src/dropbox/Dropbox.java
+++ TheSocialFramework/src/dropbox/Dropbox.java
@@ -4,66 +4,85 @@ package dropbox;
 import com.dropbox.core.*;
 import java.io.*;
 import java.util.Locale;
-
+/**
+ * Permet l'utilisateur d'avoir accès à son contenu sur son compte Dropbox
+ * @author montalda
+ *
+ */
 public class Dropbox {
+
+
+	public DbxClient client;
+/**
+ * Identification pour accéder à l'application de la Dropbox
+ * @throws IOException
+ * @throws DbxException
+ */
 	
-	
-	public static void main(String[] args) throws IOException, DbxException {
-     // Get your app key and secret from the Dropbox developers website.
-     //final String APP_KEY = "INSERT_APP_KEY";
-     //final String APP_SECRET = "INSERT_APP_SECRET";
-     final String APP_KEY = "tajxrbh3ovhxxxp";
-     final String APP_SECRET = "password";
+	public void identification(String APP_KEY, String APP_SECRET) throws IOException, DbxException {
 
-     DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+		DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
 
-     DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
-         Locale.getDefault().toString());
-     DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+		DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",Locale.getDefault().toString());
+		DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
 
-     // Have the user sign in and authorize your app.
-     String authorizeUrl = webAuth.start();
-     System.out.println("1. Go to: " + authorizeUrl);
-     System.out.println("2. Click \"Allow\" (you might have to log in first)");
-     System.out.println("3. Copy the authorization code.");
-     String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+		// Have the user sign in and authorize your app.
+		String authorizeUrl = webAuth.start();
+		System.out.println("1. Go to: " + authorizeUrl);
+		System.out.println("2. Click \"Allow\" (you might have to log in first)");
+		System.out.println("3. Copy the authorization code.");
+		String code = new BufferedReader(new InputStreamReader(System.in))
+				.readLine().trim();
 
-     // This will fail if the user enters an invalid authorization code.
-     DbxAuthFinish authFinish = webAuth.finish(code);
-     String accessToken = authFinish.accessToken;
+		// This will fail if the user enters an invalid authorization code.
+		DbxAuthFinish authFinish = webAuth.finish(code);
+		String accessToken = authFinish.accessToken;
 
-     DbxClient client = new DbxClient(config, accessToken);
+		client = new DbxClient(config, accessToken);
 
-     System.out.println("Linked account: " + client.getAccountInfo().displayName);
+		System.out.println("Linked account: "
+				+ client.getAccountInfo().displayName);
+
+	}
 
 
+	public void upload() {
 
+		try {
+			File inputFile = new File("working-draft.txt");
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			try {
+				DbxEntry.File uploadedFile = client.uploadFile(
+						"/magnum-opus.txt", DbxWriteMode.add(),
+						inputFile.length(), inputStream);
+				System.out.println("Uploaded: " + uploadedFile.toString());
+			} finally {
+				inputStream.close();
+			}
 
-     File inputFile = new File("working-draft.txt");
-     FileInputStream inputStream = new FileInputStream(inputFile);
-     try {
-         DbxEntry.File uploadedFile = client.uploadFile("/magnum-opus.txt",
-             DbxWriteMode.add(), inputFile.length(), inputStream);
-         System.out.println("Uploaded: " + uploadedFile.toString());
-     } finally {
-         inputStream.close();
-     }
+			DbxEntry.WithChildren listing = client.getMetadataWithChildren("/");
+			System.out.println("Files in the root path:");
+			for (DbxEntry child : listing.children) {
+				System.out.println("	" + child.name + ": " + child.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-     DbxEntry.WithChildren listing = client.getMetadataWithChildren("/");
-     System.out.println("Files in the root path:");
-     for (DbxEntry child : listing.children) {
-         System.out.println("	" + child.name + ": " + child.toString());
-     }
-     
-     
+	}
 
-     FileOutputStream outputStream = new FileOutputStream("magnum-opus.txt");
-     try {
-         DbxEntry.File downloadedFile = client.getFile("/magnum-opus.txt", null,
-             outputStream);
-         System.out.println("Metadata: " + downloadedFile.toString());
-     } finally {
-         outputStream.close();
-     }
- }
+	public void download() {
+		try {
+			FileOutputStream outputStream = new FileOutputStream("magnum-opus.txt");
+			try {
+				DbxEntry.File downloadedFile = client.getFile("/magnum-opus.txt", null, outputStream);
+				System.out.println("Metadata: " + downloadedFile.toString());
+			} finally {
+				outputStream.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
