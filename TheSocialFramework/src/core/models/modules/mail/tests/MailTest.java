b9df43a32e3a8e81b4ff045d95e8ba532d80a406
imag-soft <imag-soft@imagsoft-Latitude-E6500>
2014-04-22 14:32:10 +0200

    Corrections sur les biblis

--- TheSocialFramework/src/core/models/modules/mail/tests/MailTest.java
+++ TheSocialFramework/src/core/models/modules/mail/tests/MailTest.java
@@ -10,6 +10,7 @@ public class MailTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		EnvoyerMail.mail("theSocialFramework@laposte.net",
 				"gcharlety@laposte.net", "theSocialFramework", "Miage2014",
 				"SujetTest", "Corps  Tests éà");
